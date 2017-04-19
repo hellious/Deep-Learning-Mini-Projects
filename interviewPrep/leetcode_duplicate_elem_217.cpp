c57b6757cefbe049482172cf3f3ca85cd2b1b3d8
saurabh singh <saur_navigator@Saurabh.local>
2017-04-18 21:46:26 -0700

    Adding for finding duplicates elements

---
+++ interviewPrep/leetcode_duplicate_elem_217.cpp
@@ -0,0 +1,19 @@
+// sort the data 
+// check for consecutive element wether they are same or not
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int len = nums.size();
+        if (!len)
+            return false;
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<len-1; i++){
+            //cout<<" num 1 and 2 "<<nums[i]<<" "<<nums[i+1]<<endl;
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
